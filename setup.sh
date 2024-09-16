63bec3e770dcd4a83fcb6a3945e0cf0aa08a9747
JJ Smiley <github@grandmasterj.com>
2024-09-16 19:11:26 -0400

    Update setup.sh
    enable all automatic updates.

--- setup.sh
+++ setup.sh
@@ -132,6 +132,24 @@ defaults write com.apple.finder "FXRemoveOldTrashItems" -bool "true"
 # Reveal IP address, hostname, OS version, etc., when clicking the clock in the login window
 sudo defaults write /Library/Preferences/com.apple.loginwindow AdminHostInfo HostName
 
+# Enable the automatic update check
+defaults write com.apple.SoftwareUpdate AutomaticCheckEnabled -bool true
+
+# Check for software updates daily, not just once per week
+defaults write com.apple.SoftwareUpdate ScheduleFrequency -int 1
+
+# Download newly available updates in background
+defaults write com.apple.SoftwareUpdate AutomaticDownload -int 1
+
+# Install System data files & security updates
+defaults write com.apple.SoftwareUpdate CriticalUpdateInstall -int 1
+
+# Automatically download apps purchased on other Macs
+defaults write com.apple.SoftwareUpdate ConfigDataInstall -int 1
+
+# Turn on app auto-update
+defaults write com.apple.commerce AutoUpdate -bool true
+
 # Use column view in all Finder windows by default
 defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
 
